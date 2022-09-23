b145861e13031e65724fbc5824ffc58d12877d2a
Abyan7002 <113176132+Abyan7002@users.noreply.github.com>
2022-09-23 17:18:01 +0700

    secret_Card_1
    this is just testing i put my first coding to GitHub

---
+++ kartu rahasia Coding.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+	cout<<"Dear \n";
+	cout<<".... \n";
+	cout<<"Di mana pun kau berada \n\n";
+	
+	cout<<"Aku inginkan dirimu datang dan temui akuu...\n";
+	cout<<"'kan ku katakan padamu....\n";
+	cout<<"Selamat Ulang tahun pernikahan kalian ke 2 tahun ;>\n\n";
+	
+	cout<<"yaa..selamat ulang tahun pernikahan ke 2 tahun semoga sehat selalu :D \n\n";
+	
+	cout<<"Palembang 26 Agustus \n";
+	cout<<".... \n";
+	cout<<"";
+	
+	
+	
+}
